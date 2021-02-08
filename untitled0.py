2f67286b14100de0f00740776f51081f08871a20
Yass Fuentes <backmind@gmail.com>
2021-02-08 22:41:16 +0100

    just a git integration with spyder test

---
+++ untitled0.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 18:53:51 2021
+
+@author: backm
+"""
+
+# =============================================================================
+# virtud = 13
+# if virtud == 14:
+#     print("DE 14")
+# elif virtud == 13: # casi es 14
+#     print("CASI VIRTUOSO")
+# elif virtud == 16:
+#     print("Poca virtud")
+# else:
+#     print("Ninguna virtud")
+#     
+# =============================================================================
+
+
+a = [1,2,3]
+
+
+b = 0
+for ele in a:
+    b = b+ele
+    
+print(b)
